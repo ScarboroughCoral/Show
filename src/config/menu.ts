b325f162c7f0e41bd0db30f57449e5bdb0fc96da
ScarboroughCoral <3249977074@qq.com>
2020-03-22 18:19:18 +0800

    chore:修改目录文件配置

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -1,16 +1,16 @@
 
 export default [
   {
-    title:'技术博客',
-    desc:'用于记录一些开发学习笔记和实验记录。',
-    isRoute:false,
-    path:'http://blog.scarboroughcoral.top/',
-    img:'https://file.moetu.org/images/2020/03/12/154a1c3b35237fcf0.jpg'
+    title: '技术博客',
+    desc: '用于记录一些个人文章和实验记录。',
+    isRoute: false,
+    path: 'http://blog.scarboroughcoral.top/',
+    img: 'https://file.moetu.org/images/2020/03/12/154a1c3b35237fcf0.jpg'
   },
   {
-    title:'刷题笔记',
-    desc:'用于记录刷题思路。主要有LeetCode、Codewars、剑指offer还有一些JavaScript常见面试题。',
-    isRoute:false,
-    path:'http://code.scarboroughcoral.top/',
+    title: '刷题笔记',
+    desc: '用于记录刷题笔记。主要是算法题、前端工程问题、面试题等',
+    isRoute: false,
+    path: 'https://github.com/ScarboroughCoral/Notes',
   },
 ]
